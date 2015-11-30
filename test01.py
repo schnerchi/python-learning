bb93f7a934af40214288c2400f3b6266a1cd7505
Christoph Schnerch <christophschnerch@mac.com>
2015-11-30 10:08:40 +0100

    update 03

--- test01.py
+++ test01.py
@@ -1,6 +1,9 @@
-
 a=3
 b=3.0
-a=input("Huhu :")
-print("Eingabe war : ",a)
-print("B = ",b)
+a=input("Eingabe Nummer 1 :")
+print("Eingabe 01 war : ",a)
+b=input("Eingabe Nummer 2 : ")
+print("Eingabe 02 war ",b)
+print("Programm beendet!")
+
+
