14c944969b10edf0daedeed52262404c089d4940
Christoph Schnerch <christophschnerch@mac.com>
2015-11-30 10:15:05 +0100

    update

--- test01.py
+++ test01.py
@@ -1,9 +1,10 @@
 a=3
 b=3.0
-a=input("Eingabe Nummer 1 :")
+a=input("Eingabe Nummer 1 : ")
 print("Eingabe 01 war : ",a)
 b=input("Eingabe Nummer 2 : ")
-print("Eingabe 02 war ",b)
+print("Eingabe 02 war : ",b)
+print("a geteilt durch b : ", float(a)/float(b)) 
 print("Programm beendet!")
 
 
